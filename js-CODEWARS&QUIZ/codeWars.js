d49f9157a7f0b726ce716818bda58fa47e31c8be
Sara Mehrysabet <142097017+Sarahmsabet@users.noreply.github.com>
2023-12-01 17:27:47 +0330

    Add files via upload

---
+++ js-CODEWARS&QUIZ/codeWars.js
@@ -0,0 +1,10 @@
+// CODEWARS
+
+
+// https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/javascript
+
+
+// https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/javascript
+
+
+// https://www.codewars.com/kata/554e4a2f232cdd87d9000038/train/javascript
